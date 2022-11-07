662c87e14a400b0dad73cbc6350e7af4e28ff54d
darod <daniel_arod@protonmail.com>
2022-11-07 11:38:29 -0500

    mejorada la interfaz

--- frontend-mails/src/App.vue
+++ frontend-mails/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="wrapper">
-  <div id="top-nav">
-    <form @submit.prevent="sendRequest">
+  <nav id=navbar class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">Prueba técnica Truora</a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0" @submit.prevent="sendRequest">
       <div>
-        <label for="userId">Buscar </label>
-        <input for="text" id="userId" v-model="message">
-        <button>Enviar</button>  
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="message">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
       </div>
     </form>
+    </div>
+  </nav>
 
-  </div>
 
-  <div style="width: 600px; float: left;">
-    <table>
+
+  <div id=tablediv class="table-wrapper-scroll-y my-custom-scrollbar">
+
+    <table id="tableResponses" class="table table-striped table-bordered table-sm">
+      <thead>
       <td>Subject</td>
       <td>From</td>
       <td>To</td>
-    </table>
-    <table id="tableResponses">
+    </thead>
       <tbody>
           <tr v-for="todo in todos" :key="todo">
           <td @click="getMessage(todo._source)">{{todo._source.Subject}}</td>
@@ -31,7 +36,8 @@
       </tbody>
     </table>
   </div>
-  <div id="divMsg">{{msg}}</div>
+  <div id= msgdiv class="overflow-auto p-3 bg-light">{{msg}}</div>
+
 </div>
 </template>
 
@@ -90,32 +96,46 @@
 <style>
 #wrapper {
   width: 100%;
+  height: 100%;
   background-color: #fff;
 }
-#top-nav {
+#navbar {
   position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  height: 60px;
   width: 100%;
-  background-color: green;
+
 }
 table, th, td {
   border: 1px solid;
 }
-#entryBox{
-  padding-bottom: 3%;
+
+#msgdiv{
+  margin-left: 60%;
+  margin-top: 5%;
+  max-width: fit-content; 
+  max-height: 800px;
   position: fixed;
-}
-#divMsg{
-  margin-left: 1020px;
-  position:fixed;
-  overflow-y: auto;
   
 }
 #tableResponses{
   overflow-y: auto;
+  width: fit-content;
+  height: fit-content;
+}
+
+#tablediv{
+  margin-top: 5%;
+  width: 60%; 
+  float: left;
+  height: 800px;
+}
+
+.my-custom-scrollbar {
+position: relative;
+height: 200px;
+overflow: auto;
+}
+.table-wrapper-scroll-y {
+display: block;
 }
 
 </style>
